5d0897ab33a64844816c7ee753571ca83693fd7f
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-19 14:45:32 +0100

    feat: post rout method test

---
+++ api/clients/post.php
@@ -0,0 +1,43 @@
+<?php
+if($acao == "" && $param = ""){echo json_encode(["ERRO"=> "No way!"]); exit;}
+
+if ($acao == "adiciona" && $param == "") {
+    $data = json_decode(trim(file_get_contents("php://input")));
+    
+    foreach ($data as $key => $value) {
+        $array[$key] = $value;
+    }
+
+    $sql = "INSERT INTO clients (";
+    $contador = 1;
+    foreach (array_keys($array) as $indece){    
+        if (count($array)>$contador) {
+            $sql .= "{$indece},";
+        } else {
+            $sql .= "{$indece}";
+        }
+        $contador++;
+    }
+    $sql .= ") VALUES (";
+    $contador = 1;
+    foreach (array_values($array) as $value){    
+        if (count($array)>$contador) {
+            $sql .= "'{$value}',";
+        } else {
+            $sql .= "'{$value}'";
+        }
+        $contador++;
+    }
+    $sql .= ")";
+
+     $db = DB::connect();
+
+    $rs = $db->prepare($sql);
+    $exec = $rs->execute();
+
+    if ($exec) {
+        echo json_encode(["dado"=>"INseridos com sucesso!"]);
+    }else{
+        echo json_encode(["dado"=>"Erro inesperado!"]);
+    }
+}
