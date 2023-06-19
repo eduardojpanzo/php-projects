914f320ee9bfaee3bd5f95bf02ad93a4ab999640
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-19 13:37:35 +0100

    intial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+function noWayResponse() {
+    echo json_encode(["ERRO"=> "No way!"]);
+    exit;
+}
+
+header("Access-Control-Allow-Origin:*"); // definir o cors, que pagina poderá cosumir nossa api, neste caso (*) todos
+header("Content-type: application/json"); // A api usará retorno de json, 
+
+date_default_timezone_set("america/Sao_Paulo"); //fuso horário
+
+if (isset($_GET["path"])) {
+    $path = explode("/", $_GET["path"]);
+} else {
+    noWayResponse();
+}
+
+if (isset($path[0])) {$api = $path[0];} else {noWayResponse();}
+if (isset($path[1])) {$acao = $path[1];} else {$acao = "";}
+if (isset($path[2])) {$param = $path[2];} else {$param = "";}
+
+$method = $_SERVER["REQUEST_METHOD"]; 
+
+include_once "classes/db.class.php";
+include_once "api/clients/clients.php"
+
+?>
