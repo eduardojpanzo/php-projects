aaf36b7da68954cfa110ff9576940f7a7b9efeed
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-03-27 23:32:21 +0100

    feat: initial front page

---
+++ geral/assets/js/estadio.js
@@ -0,0 +1,128 @@
+initialEstadioView()
+
+
+async function initialEstadioView() {
+    const estadios = await getManyField("estadio");
+
+    estadios.map(
+        ({ id_estadio, nome, capacidade, localizacao }, i) => {
+            document.querySelector(".view_tbody").innerHTML += `
+            <tr data-key="${id_estadio}">
+                <th scope="row">${i + 1}</th>
+                <td>${nome}</td>
+                <td>${capacidade}</td>
+                <td>${localizacao}</td>
+                <td class='d-flex gap-2'>
+                    <button class="btn btn-info btn-sm" onclick="handleDetails(${id_estadio})">Detalhes</button>
+                    <button class="btn btn-primary btn-sm" onclick="handleBuildModalForm(${Number(id_estadio)})">Editar</button>
+                    <button class="btn btn-danger btn-sm" onclick="handleDeleteItem(${id_estadio})">Remover</button>
+                </td>
+            </tr>
+        `;
+        }
+    );
+}
+
+async function handleDetails(id) {
+    const item = await getOneField("estadio", id)
+
+    modalOverlay.querySelector(".modal-content").innerHTML = `
+    <div class="card">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+
+        <div class="card-body">
+            <h5 class="card-title">Nome: ${item.
+            nome}</h5>
+            <p class="card-text">Localizacão: ${item.localizacao}</p>
+            <p>Capacidade: <span class="badge bg-light text-dark">${item.capacidade ?? 0} pessoas</span></p>
+            Equipa:
+            <div class="d-flex gap-1 ">
+            ${item?.equipa ? `<span class="badge bg-secondary">${equipa.nome}</span>` : ''}
+            </div>
+        </div>
+    </div>`
+    openModal();
+}
+
+async function handleBuildModalForm(id) {
+    let item;
+
+    if (typeof (id) === 'number') {
+        item = await getOneField("estadio", id)
+    }
+    const formTamplete = `
+    <h4 class="fw-bold text-center mb-3">${id ? 'Editar' : 'Novo'} Estádio</h4>
+
+    <form  id="registrationForm" class="row g-2 needs-validation" novalidate onsubmit="handleValidation(event)">
+        ${id && `<input type='hidden' id='id' name='id' value='${id}'>`}
+        <div class="col-md-6">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome" value="${item?.nome ?? ''}" required>
+            <span class="invalid-feedback">Por favor, insira o nome.</span>
+        </div>
+
+        <div class="col-md-6">
+            <label for="localizacao" class="form-label">Localizacão</label>
+            <input type="text" class="form-control" id="localizacao" name="localizacao" value="${item?.localizacao ?? ''}" required>
+            <span class="invalid-feedback">Por favor, insira o localizacão.</span>
+        </div>
+
+        <div class="col-md-6">
+            <label for="capacidade" class="form-label">Capacidade</label>
+            <input type="number" class="form-control" id="capacidade" name="capacidade" value="${item?.capacidade ?? ''}" required>
+            <span class="invalid-feedback">Por favor, insira o anos de capacidade.</span>
+        </div>
+        
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">${id ? 'Atualizar' : 'Criar'}</button>
+        </div>
+    </form>`
+
+    modalOverlay.querySelector(".modal-content").innerHTML = formTamplete;
+    openModal();
+}
+
+async function handleValidation(event) {
+    event.preventDefault()
+
+    if (!event.target.checkValidity()) {
+        event.target.classList.add("was-validated")
+        return
+    }
+
+    const id = Number(event.target.id.value);
+
+    if (id) {
+        await handleEditItem(event.target, id)
+        return
+    }
+
+    await handlecriarItem(event.target);
+    return
+}
+
+async function handlecriarItem(form) {
+    const itemData = {
+        nome: form.nome.value,
+        capacidade: form.capacidade.value,
+        localizacao: form.localizacao.value,
+    };
+    await postNewField("estadio", itemData);
+    window.location.reload();
+}
+
+async function handleEditItem(form, id) {
+    const itemData = {
+        nome: form.nome.value,
+        capacidade: form.capacidade.value,
+        localizacao: form.localizacao.value,
+    };
+
+    await updateField("estadio", id, itemData);
+    window.location.reload();
+}
+
+async function handleDeleteItem(id) {
+    await deleteField("estadio", id)
+    window.location.reload();
+}
---
+++ geral/assets/js/inscricao.js
@@ -0,0 +1,163 @@
+initialInscricaoView()
+// Estudar essa possibilidade de apenas aprovar no dashbord e ser criado no site principal
+
+async function initialInscricaoView() {
+    const inscricoes = await getManyField("inscricao");
+    const campeonatos = await getManyField("campeonato");
+
+    inscricoes.map(
+        ({ id_inscricao, nome_campeonato, nome_equipa, estado }, i) => {
+            document.querySelector(".view_tbody").innerHTML += `
+            <tr data-key="${id_inscricao}">
+                <th scope="row">${i + 1}</th>
+                <td>${nome_campeonato}</td>
+                <td>${nome_equipa}</td>
+                <td>${estado == 1 ? 'Aprovado' : 'Não aprovado'}</td>
+                <td class='d-flex gap-2'>
+                   ${estado == 0 ? `<button class="btn btn-warning btn-sm" onclick="handleApprove(${id_inscricao})">Aprovar</button>` : ''}
+                    <button class="btn btn-danger btn-sm" onclick="handleDeleteItem(${id_inscricao})">Remover</button>
+                </td>
+            </tr>
+        `;
+        }
+    );
+
+    document.querySelector(".filter-section").innerHTML = `
+    <form  id="aproveForm" class="row g-2 needs-validation" novalidate onsubmit="handleFilterByCampeonato(event)">
+        <div class="col-md-6">
+            <label for="id_campeonato" class="form-label">Campeonatos</label>
+            <select class="form-select" id="id_campeonato" required>
+                <option selected disabled value="">Escolher...</option>
+                ${campeonatos.map((campeonato) => {
+        return `<option  value=${campeonato.id_campeonato}>${campeonato.nome}</option>`;
+    })
+            .join("")}
+            </select>
+            <span class="invalid-feedback">Por favor, selecione um campeonato</span>
+        </div>
+
+        <div>
+            <button type="submit" class="btn btn-primary">Buscar</button>
+        </div>
+    </form>
+    `;
+}
+
+async function handleFilterByCampeonato(event) {
+    event.preventDefault()
+
+    if (!event.target.checkValidity()) {
+        event.target.classList.add("was-validated")
+        return
+    }
+
+    const id = event.target.id_campeonato.value;
+    if (id) {
+        const inscricoes = await getOneField("inscricao", id)
+
+        document.querySelector(".view_tbody").innerHTML = "";
+
+        inscricoes.map(
+            ({ id_inscricao, nome_campeonato, nome_equipa, estado }, i) => {
+                document.querySelector(".view_tbody").innerHTML += `
+                <tr data-key="${id_inscricao}">
+                    <th scope="row">${i + 1}</th>
+                    <td>${nome_campeonato}</td>
+                    <td>${nome_equipa}</td>
+                    <td>${estado == 1 ? 'Aprovado' : 'Não aprovado'}</td>
+                    <td class='d-flex gap-2'>
+                       ${estado == 0 ? `<button class="btn btn-warning btn-sm" onclick="handleApprove(${id_inscricao})">Aprovar</button>` : ''}
+                        <button class="btn btn-danger btn-sm" onclick="handleDeleteItem(${id_inscricao})">Remover</button>
+                    </td>
+                </tr>
+            `;
+            }
+        );
+        return
+    }
+
+    alert("Informação não ecotrada, verifica a escolha");
+}
+
+async function handleBuildModalForm() {
+    const equipas = await getManyField("equipa")
+    const campeonatos = await getManyField("campeonato")
+
+    const formTamplete = `
+    <h4 class="fw-bold text-center mb-3">Fazer uma Inscrição</h4>
+
+    <form  id="registrationForm" class="row g-2 needs-validation" novalidate onsubmit="handleValidation(event)">
+        <div class="col-md-6">
+            <label for="id_campeonato" class="form-label">Campeonatos</label>
+            <select class="form-select" id="id_campeonato" required>
+                <option selected disabled value="">Escolher...</option>
+                ${campeonatos.map((campeonato) => {
+        return `<option  value=${campeonato.id_campeonato}>${campeonato.nome}</option>`;
+    })
+            .join("")}
+            </select>
+            <span class="invalid-feedback">Por favor, selecione um campeonato</span>
+        </div>
+
+        <div class="col-md-6">
+            <label for="id_equipa" class="form-label">equipas</label>
+            <select class="form-select" id="id_equipa" required>
+                <option selected disabled value="">Escolher...</option>
+                ${equipas.map((equipa) => {
+                return `<option  value=${equipa.id_equipa}>${equipa.nome}</option>`;
+            })
+            .join("")}
+            </select>
+            <span class="invalid-feedback">Por favor, selecione um equipa</span>
+        </div>
+
+        <div class="mb-3 form-check form-switch">
+            <label class="form-check-label" for="estado">Pagamento da inscrição</label>
+            <input class="form-check-input" type="checkbox" id="estado">
+          </div>
+        
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Finalizar</button>
+        </div>
+    </form>`
+
+    modalOverlay.querySelector(".modal-content").innerHTML = formTamplete;
+    openModal();
+}
+
+async function handleValidation(event) {
+    event.preventDefault()
+
+    if (!event.target.checkValidity()) {
+        event.target.classList.add("was-validated")
+        return
+    }
+
+    await handlecriarItem(event.target);
+    return
+}
+
+async function handlecriarItem(form) {
+    const itemData = {
+        id_campeonato: form.id_campeonato.value,
+        id_equipa: form.id_equipa.value,
+        estado: form.estado.checked ? 1 : 0,
+    };
+
+    await postNewField("inscricao", itemData);
+    window.location.reload();
+}
+
+async function handleApprove(id) {
+    const itemData = {
+        estado: 1,
+    };
+
+    await updateField("inscricao", id, itemData);
+    window.location.reload();
+}
+
+async function handleDeleteItem(id) {
+    await deleteField("inscricao", id)
+    window.location.reload();
+}
